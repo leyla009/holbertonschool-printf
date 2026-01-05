0a3620b1295a51106d43fa63948fa75305942477
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 22:29:06 +0000

    handle field width for non-custom conversion specifiers

--- print_int.c
+++ print_int.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_int - Prints an integer with width padding
- * @args: va_list
- * @flags: flags (+, space)
- * @length: length (l, h)
+ * print_int - Prints an integer with field width, flags, and length modifiers
+ * @args: va_list of arguments
+ * @flags: active flags (+, space)
  * @width: field width
- * Return: number of chars printed
+ * @length: length modifier (l, h)
+ * Return: number of characters printed
  */
 int print_int(va_list args, int flags, int length, int width)
 {
@@ -14,15 +14,18 @@ int print_int(va_list args, int flags, int length, int width)
 	unsigned long int num, temp, div = 1;
 	int count = 0, n_len = 0;
 
-	/* 1. Handle length modifiers */
-	if (length == 1) n = va_arg(args, long int);
-	else if (length == 2) n = (short int)va_arg(args, int);
-	else n = va_arg(args, int);
+	/* 1. Extract the number with length modifier logic */
+	if (length == 1)
+		n = va_arg(args, long int);
+	else if (length == 2)
+		n = (short int)va_arg(args, int);
+	else
+		n = va_arg(args, int);
 
 	num = (n < 0) ? -n : n;
 	temp = num;
 
-	/* 2. Calculate length of the number digits */
+	/* 2. Calculate digit length */
 	if (num == 0)
 		n_len = 1;
 	else
@@ -34,26 +37,29 @@ int print_int(va_list args, int flags, int length, int width)
 		}
 	}
 
-	/* 3. Account for sign/space in width */
+	/* 3. Account for sign/space length BEFORE printing padding */
 	if (n < 0 || (flags & 1) || (flags & 2))
 		n_len++;
 
-	/* 4. Print Padding (Spaces) */
+	/* 4. Print Width Padding (Spaces) first for right alignment */
 	while (width > n_len)
 	{
 		count += _putchar(' ');
 		width--;
 	}
 
-	/* 5. Print Sign */
-	if (n < 0) count += _putchar('-');
-	else if (flags & 1) count += _putchar('+');
-	else if (flags & 2) count += _putchar(' ');
+	/* 5. Print the Sign (if any) */
+	if (n < 0)
+		count += _putchar('-');
+	else if (flags & 1)
+		count += _putchar('+');
+	else if (flags & 2)
+		count += _putchar(' ');
 
-	/* 6. Print Digits */
+	/* 6. Print the actual digits */
 	while (num / div > 9)
 		div *= 10;
-	
+
 	while (div != 0)
 	{
 		count += _putchar('0' + (num / div));
