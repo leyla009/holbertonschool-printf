a6ae0250375a3a3684bffe309897c7c009daa5bc
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 22:38:58 +0000

    handle field width for non-custom conversion specifiers

--- print_int.c
+++ print_int.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_int - Prints an integer with width, flags, and length modifiers
+ * print_int - Prints an integer with width padding
  * @args: va_list
- * @flags: active flags (+, space)
+ * @flags: flags (+, space)
  * @length: length (l, h)
  * @width: field width
- * Return: number of characters printed
+ * Return: total number of chars printed
  */
 int print_int(va_list args, int flags, int length, int width)
 {
@@ -14,55 +14,39 @@ int print_int(va_list args, int flags, int length, int width)
 	unsigned long int num, temp, div = 1;
 	int count = 0, n_len = 0;
 
-	/* 1. Get the number using the length modifier */
-	if (length == 1)
-		n = va_arg(args, long int);
-	else if (length == 2)
-		n = (short int)va_arg(args, int);
-	else
-		n = va_arg(args, int);
+	if (length == 1) n = va_arg(args, long int);
+	else if (length == 2) n = (short int)va_arg(args, int);
+	else n = va_arg(args, int);
 
 	num = (n < 0) ? -n : n;
 	temp = num;
 
-	/* 2. Calculate digit length (must be at least 1 for '0') */
-	if (num == 0)
-		n_len = 1;
-	while (temp > 0)
-	{
-		temp /= 10;
-		n_len++;
-	}
+	/* Calculate number of digits */
+	if (num == 0) n_len = 1;
+	while (temp > 0) { temp /= 10; n_len++; }
 
-	/* 3. Account for sign/space in the content length */
-	if (n < 0 || (flags & 1) || (flags & 2))
-		n_len++;
+	/* Add 1 for the sign character (+, -, or space) */
+	if (n < 0 || (flags & 1) || (flags & 2)) n_len++;
 
-	/* 4. Print Width Padding (Spaces) BEFORE anything else */
+	/* Print Padding (Spaces) BEFORE the number */
 	while (width > n_len)
 	{
 		count += _putchar(' ');
 		width--;
 	}
 
-	/* 5. Print the Sign/Space flag */
-	if (n < 0)
-		count += _putchar('-');
-	else if (flags & 1)
-		count += _putchar('+');
-	else if (flags & 2)
-		count += _putchar(' ');
+	/* Print the Sign */
+	if (n < 0) count += _putchar('-');
+	else if (flags & 1) count += _putchar('+');
+	else if (flags & 2) count += _putchar(' ');
 
-	/* 6. Print the actual number digits */
-	while (num / div > 9)
-		div *= 10;
-
+	/* Print the actual digits */
+	while (num / div > 9) div *= 10;
 	while (div != 0)
 	{
 		count += _putchar('0' + (num / div));
 		num %= div;
 		div /= 10;
 	}
-
 	return (count);
 }
--- unsigned_functions.c
+++ unsigned_functions.c
@@ -1,46 +1,17 @@
 #include "main.h"
 
-/**
- * get_unsigned_val - Fetches an unsigned number based on length modifier
- * @args: va_list of arguments
- * @len: Length modifier (1 for l, 2 for h, 0 for default)
- * Return: The number as an unsigned long int
- */
-unsigned long int get_unsigned_val(va_list args, int len)
+int print_unsigned(va_list args, int flags, int length, int width)
 {
-	if (len == 1)
-		return (va_arg(args, unsigned long int));
-	if (len == 2)
-		return ((unsigned short int)va_arg(args, unsigned int));
-	return (va_arg(args, unsigned int));
-}
-
-/**
- * print_unsigned - Prints an unsigned integer with field width
- * @args: va_list
- * @flags: Active flags (unused for u)
- * @len: Length modifier
- * @width: Field width
- * Return: Number of characters printed
- */
-int print_unsigned(va_list args, int flags, int len, int width)
-{
-	unsigned long int n = get_unsigned_val(args, len);
+	unsigned long int n = get_unsigned_val(args, length);
 	unsigned long int div = 1, temp = n;
 	int count = 0, n_len = 0;
-
 	(void)flags;
 
 	/* Calculate content length */
-	if (n == 0)
-		n_len = 1;
-	while (temp > 0)
-	{
-		temp /= 10;
-		n_len++;
-	}
+	if (n == 0) n_len = 1;
+	while (temp > 0) { temp /= 10; n_len++; }
 
-	/* Print leading spaces for width */
+	/* Print spaces for width padding */
 	while (width > n_len)
 	{
 		count += _putchar(' ');
@@ -48,152 +19,13 @@ int print_unsigned(va_list args, int flags, int len, int width)
 	}
 
 	/* Print digits */
-	while (n / div > 9)
-		div *= 10;
-
+	if (n == 0) return (count + _putchar('0'));
+	while (n / div > 9) div *= 10;
 	while (div != 0)
 	{
 		count += _putchar('0' + (n / div));
 		n %= div;
 		div /= 10;
 	}
-	return (count);
-}
-
-/**
- * print_octal - Prints an unsigned int in octal with width and # flag
- */
-int print_octal(va_list args, int flags, int len, int width)
-{
-	unsigned long int n = get_unsigned_val(args, len);
-	unsigned long int temp = n;
-	int i = 0, count = 0, n_len = 0;
-	char octal[32];
-
-	/* Calculate content length */
-	if (n == 0)
-		n_len = 1;
-	while (temp > 0)
-	{
-		temp /= 8;
-		n_len++;
-	}
-	if (n != 0 && (flags & 4))
-		n_len++;
-
-	/* Padding */
-	while (width > n_len)
-	{
-		count += _putchar(' ');
-		width--;
-	}
-
-	if (n != 0 && (flags & 4))
-		count += _putchar('0');
-
-	if (n == 0)
-		return (count + _putchar('0'));
-
-	while (n > 0)
-	{
-		octal[i++] = (n % 8) + '0';
-		n /= 8;
-	}
-	for (i--; i >= 0; i--)
-		count += _putchar(octal[i]);
-	return (count);
-}
-
-/**
- * print_hex_low - Prints hex in lowercase with width and # flag
- */
-int print_hex_low(va_list args, int flags, int len, int width)
-{
-	unsigned long int n = get_unsigned_val(args, len);
-	unsigned long int temp = n;
-	int i = 0, count = 0, n_len = 0;
-	char hex[32], *lookup = "0123456789abcdef";
-
-	/* Calculate length */
-	if (n == 0)
-		n_len = 1;
-	while (temp > 0)
-	{
-		temp /= 16;
-		n_len++;
-	}
-	if (n != 0 && (flags & 4))
-		n_len += 2;
-
-	/* Padding */
-	while (width > n_len)
-	{
-		count += _putchar(' ');
-		width--;
-	}
-
-	if (n != 0 && (flags & 4))
-	{
-		count += _putchar('0');
-		count += _putchar('x');
-	}
-
-	if (n == 0)
-		return (count + _putchar('0'));
-
-	while (n > 0)
-	{
-		hex[i++] = lookup[n % 16];
-		n /= 16;
-	}
-	for (i--; i >= 0; i--)
-		count += _putchar(hex[i]);
-	return (count);
-}
-
-/**
- * print_hex_upp - Prints hex in uppercase with width and # flag
- */
-int print_hex_upp(va_list args, int flags, int len, int width)
-{
-	unsigned long int n = get_unsigned_val(args, len);
-	unsigned long int temp = n;
-	int i = 0, count = 0, n_len = 0;
-	char hex[32], *lookup = "0123456789ABCDEF";
-
-	/* Calculate length */
-	if (n == 0)
-		n_len = 1;
-	while (temp > 0)
-	{
-		temp /= 16;
-		n_len++;
-	}
-	if (n != 0 && (flags & 4))
-		n_len += 2;
-
-	/* Padding */
-	while (width > n_len)
-	{
-		count += _putchar(' ');
-		width--;
-	}
-
-	if (n != 0 && (flags & 4))
-	{
-		count += _putchar('0');
-		count += _putchar('X');
-	}
-
-	if (n == 0)
-		return (count + _putchar('0'));
-
-	while (n > 0)
-	{
-		hex[i++] = lookup[n % 16];
-		n /= 16;
-	}
-	for (i--; i >= 0; i--)
-		count += _putchar(hex[i]);
 	return (count);
 }
