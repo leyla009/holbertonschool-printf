ea5fb4f6ed4d6f106bf45cafd848c764e8f22867
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 15:13:21 +0000

    Fix _printf.c related issues

--- _printf.c
+++ _printf.c
@@ -1,81 +1,50 @@
 #include "main.h"
 
 /**
- * handle_specifier - Matches a specifier with a function
- * @spec: The specifier character
- * @args: The arguments list
- * Return: Number of characters printed
- */
-int handle_specifier(char spec, va_list args)
-{
-	spec_t specs[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{0, NULL}
-	};
-	int i = 0;
-
-	while (specs[i].spec)
-	{
-		if (specs[i].spec == spec)
-			return (specs[i].f(args));
-		i++;
-	}
-
-	/* If no match is found, print the % and the unknown character */
-	write(1, "%", 1);
-	write(1, &spec, 1);
-	return (2);
-}
-
-/**
  * _printf - custom printf function
  * @format: format string
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0, i = 0;
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-
-	va_start(args, format);
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			count += handle_specifier(format[i + 1], args);
-			i++; /* skip the specifier character */
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		i++;
-	}
-	va_end(args);
-	return (count);
-}
-int _printf(const char *format, ...)
-{
-    /* 1. Define the array of available specifiers */
+    va_list args;
+    int i = 0, j, count = 0;
     spec_t specs[] = {
-        {'c', print_char},
-        {'s', print_string},
-        {'%', print_percent},
-        {'d', print_int},
-        {'i', print_int},
-        {0, NULL} /* Struct terminator */
+        {'c', print_char}, {'s', print_string}, {'%', print_percent},
+        {'d', print_int}, {'i', print_int}, {0, NULL}
     };
 
-    /* ... rest of your _printf logic ... */
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+
+    va_start(args, format);
+    while (format && format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            for (j = 0; specs[j].spec; j++)
+            {
+                if (format[i] == specs[j].spec)
+                {
+                    count += specs[j].f(args);
+                    break;
+                }
+            }
+            if (!specs[j].spec) /* No match found */
+            {
+                write(1, &format[i - 1], 1);
+                write(1, &format[i], 1);
+                count += 2;
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
+            count++;
+        }
+        i++;
+    }
+    va_end(args);
+    return (count);
 }
--- main.h
+++ main.h
@@ -5,24 +5,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/**
- * struct specifier - Struct for specifiers
- * @spec: The specifier (e.g., 'c', 's')
- * @f: The function associated
- */
 typedef struct specifier
 {
-	char spec;
-	int (*f)(va_list);
+    char spec;
+    int (*f)(va_list);
 } spec_t;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-
-/* Handler prototypes */
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_int(va_list args);
+int recursive_int(unsigned int num); /* Add this line */
 
 #endif
