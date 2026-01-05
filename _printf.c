275500a9abdecc0ba1fbba8996bf8339c5d9c45b
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 21:32:04 +0000

    handle flag characters +, space, and # for numeric specifiers

--- _printf.c
+++ _printf.c
@@ -22,30 +22,46 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
-		{
-			flags = 0;
-			while (format[i + 1] == '+' || format[i + 1] == ' ' || format[i + 1] == '#')
-			{
-				i++;
-				if (format[i] == '+') flags |= 1;
-				if (format[i] == ' ') flags |= 2;
-				if (format[i] == '#') flags |= 4;
-			}
-			i++;
-			for (j = 0; types[j].spec != 0; j++)
-			{
-				if (format[i] == types[j].spec)
-				{
-					count += types[j].f(args, flags);
-					break;
-				}
-			}
-			if (types[j].spec == 0)
-			{
-				count += _putchar('%');
-				count += _putchar(format[i]);
-			}
-		}
+{
+    flags = 0;
+    /* Move to next char to check for flags */
+    while (format[i + 1] == '+' || format[i + 1] == ' ' || format[i + 1] == '#')
+    {
+        i++;
+        if (format[i] == '+') flags |= 1;
+        if (format[i] == ' ') flags |= 2;
+        if (format[i] == '#') flags |= 4;
+    }
+
+    /* If the flag was a space and the NEXT char is NOT a specifier */
+    if (format[i] == ' ' && !format[i + 1])
+        return (-1);
+
+    i++; /* Move to the character that SHOULD be a specifier */
+    
+    for (j = 0; types[j].spec != 0; j++)
+    {
+        if (format[i] == types[j].spec)
+        {
+            count += types[j].f(args, flags);
+            break;
+        }
+    }
+    
+    if (types[j].spec == 0) /* No valid specifier found */
+    {
+        if (format[i] == '\0')
+            return (-1);
+        
+        /* If it was just "% ", we need to print the '%' and the ' ' */
+        count += _putchar('%');
+        if (format[i - 1] == ' ')
+            count += _putchar(' ');
+        
+        if (format[i] != ' ')
+            count += _putchar(format[i]);
+    }
+}
 		else
 			count += _putchar(format[i]);
 	}
