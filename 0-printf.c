88626032710cabfb73b64d01c81af2ad55f1bd9a
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 14:38:05 +0000

    Fix 0-printf.c related issues

--- 0-printf.c
+++ 0-printf.c
@@ -1,61 +1,64 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
+ * _printf - prints formatted output to stdout
  * @format: format string
  *
- * Return: number of characters printed, or -1 on error
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int count;
-	unsigned int i;
-	int (*f)(va_list);
+    va_list args;
+    int count = 0;
+    const char *p;
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
+    if (!format)
+        return (-1);
 
-	va_start(ap, format);
+    va_start(args, format);
 
-	count = 0;
-	i = 0;
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			++count;
-			++i;
-			continue;
-		}
+    for (p = format; *p; p++)
+    {
+        if (*p == '%')
+        {
+            p++; /* move to the next character */
 
-		++i;
+            if (!*p) /* if % is the last character, do nothing */
+                break;
 
-		if (format[i] == '\0')
-		{
-			va_end(ap);
-			return (-1);
-		}
+            if (*p == 'c')
+            {
+                char c = (char)va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*p == 's')
+            {
+                char *s = va_arg(args, char *);
+                if (!s)
+                    s = "(null)";
+                while (*s)
+                {
+                    write(1, s, 1);
+                    s++;
+                    count++;
+                }
+            }
+            else if (*p == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            /* unknown specifiers are ignored completely */
+        }
+        else
+        {
+            write(1, p, 1);
+            count++;
+        }
+    }
 
-		f = get_specifier(format[i]);
-
-		if (f != NULL)
-		{
-			count += f(ap);
-		}
-		else
-		{
-			_putchar('%');
-			_putchar(format[i]);
-			count += 2;
-		}
-
-		++i;
-	}
-
-	va_end(ap);
-	return (count);
+    va_end(args);
+    return (count);
 }
+
