63da5fff52abd83ecd9ff054406895554a855b01
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 20:06:43 +0000

    Fix _printf.c issues

--- _printf.c
+++ _printf.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
- * _printf - custom printf function that produces output according to a format
- * @format: character string containing zero or more directives
- *
- * Return: the number of characters printed (excluding the null byte)
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, j, count = 0;
-	spec_t specs[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_int},
-		{'i', print_int},
-		{'b', print_binary},
-		{0, NULL}
+	spec_t types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'d', print_int}, {'i', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal},
+		{'x', print_hex_low}, {'X', print_hex_upp}, {0, NULL}
 	};
 
 	if (!format || (format[0] == '%' && !format[1]))
@@ -29,26 +25,22 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			for (j = 0; specs[j].spec; j++)
+			for (j = 0; types[j].spec != 0; j++)
 			{
-				if (format[i] == specs[j].spec)
+				if (format[i] == types[j].spec)
 				{
-					count += specs[j].f(args);
+					count += types[j].f(args);
 					break;
 				}
 			}
-			if (!specs[j].spec)
+			if (types[j].spec == 0) /* No match found */
 			{
-				write(1, &format[i - 1], 1);
-				write(1, &format[i], 1);
-				count += 2;
+				count += _putchar('%');
+				count += _putchar(format[i]);
 			}
 		}
 		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
+			count += _putchar(format[i]);
 		i++;
 	}
 	va_end(args);
--- _putchar.c
+++ _putchar.c
@@ -1,48 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _printf - custom printf function
- * @format: format string
- * Return: number of characters printed
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned.
  */
-int _printf(const char *format, ...)
+int _putchar(char c)
 {
-	int i = 0, j, printed_chars = 0;
-	va_list args;
-	spec_t func_list[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'d', print_int}, {'i', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal},
-		{'x', print_hex_low}, {'X', print_hex_upp}, {'\0', NULL}
-	};
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-
-	va_start(args, format);
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++; /* Move to specifier */
-			for (j = 0; func_list[j].spec != '\0'; j++)
-			{
-				if (format[i] == func_list[j].spec)
-				{
-					printed_chars += func_list[j].f(args);
-					break;
-				}
-			}
-			if (func_list[j].spec == '\0') /* No match found */
-			{
-				printed_chars += _putchar('%');
-				printed_chars += _putchar(format[i]);
-			}
-		}
-		else
-			printed_chars += _putchar(format[i]);
-		i++;
-	}
-	va_end(args);
-	return (printed_chars);
+	return (write(1, &c, 1));
 }
