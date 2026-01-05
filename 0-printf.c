595faaa5977b15ccb1a50234ce04eccef572a976
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 12:35:42 +0000

    Add 0-printf.c

---
+++ 0-printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _printf - prints formatted output
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+    const char *p;
+
+    if (!format)
+        return (-1);
+
+    va_start(args, format);
+
+    for (p = format; *p; p++)
+    {
+        if (*p == '%')
+        {
+            p++;
+            if (*p == 'c')
+            {
+                char c = (char)va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*p == 's')
+            {
+                char *s = va_arg(args, char *);
+                if (!s)
+                    s = "(null)";
+                while (*s)
+                {
+                    write(1, s, 1);
+                    s++;
+                    count++;
+                }
+            }
+            else if (*p == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else
+            {
+                /* For unknown specifiers, just print % and the character */
+                write(1, "%", 1);
+                write(1, p, 1);
+                count += 2;
+            }
+        }
+        else
+        {
+            write(1, p, 1);
+            count++;
+        }
+    }
+
+    va_end(args);
+    return (count);
+}
+
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* Prototype for _printf */
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
+
